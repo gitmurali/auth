6d4755e662edffc468c64b958ed4cea23906ac60
Murali Nutalapati <info@muraliprashanth.me>
2017-08-19 16:45:17 +0530

    encrypts the passwords.

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,25 @@
 var mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 // Define user model
 const userSchema = new Schema({
     email: { type:String, unique: true, lowercase: true },
     password: String
 })
+
+//on save hook encrypt password
+userSchema.pre('save', function(next) {
+   const user = this;
+   bcrypt.genSalt(10, function(err, salt) {
+        if(err){return next(err);}
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if(err) {return next(err);}
+            user.password = hash;
+            next(); // go to next save function..
+        });
+   });
+});
 
 // Define model class
 const ModelClass = mongoose.model('user', userSchema);
