ada05f374177c280be67913bfb5439a2b6f4427a
Murali Nutalapati <info@muraliprashanth.me>
2017-08-21 01:23:03 +0530

    authentication setup at server level

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,15 @@
+const jwt = require('jwt-simple');
+const config = require('../config');
 const User = require('../models/user');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+    res.send({token: tokenForUser(req.user)});
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -21,8 +32,7 @@ exports.signup = function(req, res, next) {
         user.save(function(err) {
             if(err){return next(err);}
             //res.json(user);
-            res.send({ success: true })
+            res.send({ token: tokenForUser(user) });
         })
-
     })
 }
--- server/router.js
+++ server/router.js
@@ -1,5 +1,14 @@
 const authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {session:false});
+const requireSignIn = passport.authenticate('local', {session:false});
 
 module.exports = function(app) {
+    app.get('/', requireAuth, function (req, res) {
+       res.send({hi:'there'});
+    });
+    app.post('/signin', requireSignIn, authentication.signin);
 	app.post('/signup', authentication.signup);
 }
